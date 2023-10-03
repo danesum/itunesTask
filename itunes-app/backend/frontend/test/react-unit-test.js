1675dfd07a724e3fbdb093ce41066a2c52165933
danesum <135839788+danesum@users.noreply.github.com>
2023-10-03 13:54:48 +0200

    Delete itunes-app/backend directory
    Pre update

--- itunes-app/backend/frontend/test/react-unit-test.js
+++
@@ -1,7 +0,0 @@
-let encodedTerm = encodeURI("taylor swift");
-let searchCategory = "music";
-test("Function for forming URLs work", () => {
-  expect(`/search/?search=${encodedTerm}&category=${searchCategory}`).toBe(
-    "/search/?search=taylor%20swift&category=music"
-  );
-});
